cfc78532d44d1f0e99598391144dbf945025a9e8
ItelSunday <iteldomingotech@gmail.com>
2019-03-22 10:13:09 -0700

    added get for projectRouter

--- data/routers/projectRouters.js
+++ data/routers/projectRouters.js
@@ -3,6 +3,15 @@ const router = express.Router();
 
 const db = require('../helpers/projectModel');
 
-
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+        db
+        .get(id)
+        .then(project => {
+            console.log(project);
+            res.status(200).json(project);
+        })
+        .catch();
+});
 
 module.exports = router;
