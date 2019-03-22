8026dd7eae120e6ead128c74e36e36ec9ffed1c8
ItelSunday <iteldomingotech@gmail.com>
2019-03-22 09:15:51 -0700

    added middleware and dependencies

--- api/server.js
+++ api/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const helmet = require('helmet');
+const cors = require('cors');
+const morgan = require('morgan');
 
-const db = require('../data/dbConfig.js');
+// const db = require('../data/dbConfig.js');
 
 const server = express();
 
-server.use(helmet());
 server.use(express.json());
+server.use(helmet());
+server.use(cors());
+server.use(morgan());
+
 
 
 
